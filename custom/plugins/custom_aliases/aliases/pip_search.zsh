498b4a9d4489b7a42ef879f808cfd39b6662f1b3
Elyssy <elyssy1071@protonmail.com>
2023-12-22 08:22:36 +0300

    add also pip3 into aliases

--- custom/plugins/custom_aliases/aliases/pip_search.zsh
+++ custom/plugins/custom_aliases/aliases/pip_search.zsh
@@ -1,6 +1,9 @@
-alias pip='function _pip(){
+local _pip='function _pip(){
     if [ "$1" = "search" ]; then
         pip_search "$2";
     else pip "$@";
     fi;
 };_pip'
+
+alias pip=$_pip
+alias pip3=$_pip
