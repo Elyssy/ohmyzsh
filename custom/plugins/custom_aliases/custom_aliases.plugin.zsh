a067c36e1e435df77a8921dd519f0041a7052209
Elyssy <elyssy1071@protonmail.com>
2023-12-22 09:16:43 +0300

    enable pipx, update /home/elyssy/.asdf/shims:/home/elyssy/.asdf/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/usr/lib/wsl/lib:/home/elyssy/.local/bin

--- custom/plugins/custom_aliases/custom_aliases.plugin.zsh
+++ custom/plugins/custom_aliases/custom_aliases.plugin.zsh
@@ -8,6 +8,10 @@
 0="${${ZERO:-${0:#$ZSH_ARGZERO}}:-${(%):-%N}}"
 0="${${(M)0:#/*}:-$PWD/$0}"
 
+# Add $HOME/.local/bin into $PATH
+if [[ ":$PATH:" != *":$HOME/.local/bin:"* ]]; then
+    export PATH="$PATH:$HOME/.local/bin"
+fi
 
 # Function to load aliases from files in a directory
 load_aliases() {
